3483e3df33b00386cf108b7ff692daa82bdab048
HairyMaclary <cliff.hamilton@outlook.com>
2018-12-13 20:59:52 +1300

    starting emitter example

---
+++ emitter.js
@@ -0,0 +1,12 @@
+// could also use a class contructor to make this object
+function Emitter() {
+    this.events = {};
+}
+
+// add an on method to the protoype of all objects create from the function constructor
+Emitter.prototype.on = (type, listener) => {
+
+}
+
+
+
--- greet.js
+++
@@ -1,8 +0,0 @@
-console.log('Dur to require(), this appears automatically in app.js');
-
-var greet = function() {
-    console.log("hello");
-};
-
-module.exports.greet = greet;
-

c9d2f4d16aeafdec0a64c42ab507c537e9f02197
Cliff Hamilton <c.hamilton@adinstruments.com>
2018-12-14 17:45:55 +1300

    still on lec 34

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const greet = require('./greet');
+const Emitter = require('events');
 
-greet.greet();
+let emtr = new Emitter();
+
+// Add some listeners
+
+// say whenever a greet happens we want to do something.
+emtr.on('greet', () => {
+    console.log('Somewhere, someone said hello.');
+});
+
+// And this is another thing to do when a greet occurs
+emtr.on('greet', () => {
+    console.log('A greeting occured!');
+});
+
+// simulate the event:
+console.log('Hello!');
+
+// let the application know that a greet event happened
+emtr.emit('greet');
--- emitter.js
+++ emitter.js
@@ -1,12 +1,35 @@
 // could also use a class contructor to make this object
+// we want to be able to create multiple emitters.
 function Emitter() {
     this.events = {};
 }
 
-// add an on method to the protoype of all objects create from the function constructor
-Emitter.prototype.on = (type, listener) => {
+// add an "on" method to the protoype of all objects created from the function constructor
+Emitter.prototype.on = function (type, listener) {
 
+    //if the type property already exists then good, otherwise make a new array.
+    this.events[type] = this.events[type] || [];
+    
+    // now push the listener function into the array
+    // we are building up an array of functions. One array for each event type.
+    // onBlah: [function() {...}, function() {...}, ... }
+    this.events[type].push(listener);
 }
+
+// now we want to say that something happened; we emit an event.
+Emitter.prototype.emit = function (type) {
+    // If I have the type of event on my object, i'll loop over the 
+    // associated array and execute each listener function
+    if(this.events[type]) {
+        this.events[type].forEach(listener => {
+            listener();
+        })
+    }
+}
+
+module.exports = Emitter;
+
+
 
 
 
